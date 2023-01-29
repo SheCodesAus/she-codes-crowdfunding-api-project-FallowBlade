82f0af68a79881087aa1d1a0f2d738da95cbd7af
FallowBlade <113985916+FallowBlade@users.noreply.github.com>
2023-01-29 17:48:47 +0800

    Updated settings.py with pagination
    paginated the framework so that if count of values = >5, then add the next 5 values to a new page.

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -142,7 +142,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        
-    ]
+        'rest_framework.authentication.SessionAuthentication',],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
 }
